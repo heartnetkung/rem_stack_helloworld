81a568f9bc69b9460a1529d18c5848aa9858cae2
heartnetkung <heartnetkung@gmail.com>
2020-12-31 18:18:37 +0700

    init commit

---
+++ express/index.js
@@ -0,0 +1,28 @@
+const env = require("../env");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const main = async () => {
+	//mongoose
+	var mongoStatus = "not connected";
+	mongoose
+		.connect(env.mongo_url, { useNewUrlParser: true })
+		.then(() => (mongoStatus = "connected"))
+		.catch((e) => (mongoStatus = e.message));
+
+	//routes
+	var routes = {
+		"/health": (req, res) => res.send("healthy"),
+		"/hello": (req, res) => res.json({ text: "world" }),
+		"/mongo": (req, res) => res.json({ mongoStatus }),
+	};
+
+	//express
+	var app = express();
+	app.use(cors());
+	for (var route of routes) app.get(route, routes[route]);
+	app.listen(env.port);
+};
+
+if (require.main === module) main().catch((e) => console.error(e));
